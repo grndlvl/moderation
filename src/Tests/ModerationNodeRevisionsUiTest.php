352aaf372e53b3d4d7b40b66f92c090b5fa25054
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2015-11-20 10:03:32 -0500

    Begin writing revision ui tests

---
+++ src/Tests/ModerationNodeRevisionsUiTest.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\moderation\Tests\NodeRevisionsUiTest.
+ */
+
+namespace Drupal\moderation\Tests;
+
+use Drupal\Core\Url;
+use Drupal\node\Entity\Node;
+use Drupal\node\Entity\NodeType;
+use Drupal\node\Tests\AssertButtonsTrait;
+use Drupal\node\Tests\NodeTestBase;
+
+/**
+ * Tests the UI for controlling node revision behavior.
+ *
+ * @group node
+ */
+class ModerationNodeRevisionsUiTest extends NodeTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['node', 'moderation', 'block'];
+
+  protected $nid_with_revisions = 0;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    // Use revisions by default.
+    $this->nodeType = NodeType::load('article');
+    $this->nodeType->setNewRevision(TRUE);
+    $this->nodeType->save();
+
+    // Set unpublished by default.
+    $fields = \Drupal::entityManager()->getFieldDefinitions('node', 'article');
+    $fields['status']->getConfig('article')
+      ->setDefaultValue(FALSE)
+      ->save();
+
+    $this->editor = $this->drupalCreateUser([
+      'administer nodes',
+      'edit any article content',
+      'view article revisions',
+      'access user profiles',
+    ]);
+
+    $this->createRevisions();
+  }
+
+  protected function createRevisions() {
+    $this->drupalLogin($this->editor);
+
+    // Create a node.
+    $edit = array();
+    $edit['title[0][value]'] = $this->randomMachineName(8);
+    $edit['body[0][value]'] = $this->randomMachineName(16);
+    $this->drupalPostForm('node/add/article', $edit, t('Save'));
+
+    $node = $this->drupalGetNodeByTitle($edit['title[0][value]']);
+    $edit['title[0][value]'] = $this->randomMachineName(8);
+    $this->drupalPostForm('node/' . $node->id() . '/edit', $edit, t('Save as draft'));
+
+    $nid_with_revisions = $node->id();
+  }
+
+}
